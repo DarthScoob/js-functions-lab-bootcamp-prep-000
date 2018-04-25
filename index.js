14bdd9fd20d059831388011ab3f7d88514e7df63
spharris5200@gmail.com <spharris5200@gmail.com>
2018-04-25 23:37:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays() {
   return("Happy holidays!");
 }
 function happyHolidaysTo(name) {
-  return(`$name`);
+  return(`${name}`);
 }
 function happyHollidayto(holiday, name) {
-  
+  return(`Happy ${holiday}, ${name}!`);
 }
