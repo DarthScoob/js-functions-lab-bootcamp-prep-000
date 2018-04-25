cfbd961dbf26c38d97bc18f0d72821788af8d653
spharris5200@gmail.com <spharris5200@gmail.com>
2018-04-25 23:47:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ function happyHolidaysTo(name) {
   return(`Happy holidays, ${name}!`);
 }
 function happyHollidayto(holiday, name) {
-  var holiday = "Independance holiday"
-  var name = "You"
   return(`Happy ${holiday}, ${name}!`);
 }
